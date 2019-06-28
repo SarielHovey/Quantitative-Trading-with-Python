dfd2ff4cc087dc948a7ada95aefe7aff9061f2d7
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-06-28 10:13:23 +0800

    Update Package_doParallel.r

--- Package_doParallel.r
+++ Package_doParallel.r
@@ -1,1 +1,9 @@
+# the package is used to speed up calculation in R for multi-core PCs
+library(doParallel)
+threads <- 6
+# Set up how many threads to run. Im running a 6-core PC(AMD Ryzen 2600)
+registerDoParallel( cores = threads )
 
+m <- matrix(rnorm(34000), 1000, 34)
+# Parallel-process to get matrix m normalized
+m <- foreach(i=1:nrow(m), .combine=rbind) %dopar% (m[i,] / mean(m[i,]))
