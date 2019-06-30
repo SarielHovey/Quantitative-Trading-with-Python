316ebf675bcd44a77670415268e91b2b60daf555
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-06-30 16:39:26 +0800

    Update Continuous Compounding.r

--- Pricing with R/Continuous Compounding.r
+++ Pricing with R/Continuous Compounding.r
@@ -1,1 +1,14 @@
+# Used to convert between continuous compounding and compounding
+# R1 is continuous compounding interest
+# R2 is compounding interest, calculated m times a year
+cci <- function (R2, m) {
+    R1 = m * log(1 + R2 / m)
+    return(R1)
+}
+# used to convert R2 to R1
 
+
+ncci <- function (R1, m) {
+    R2 = m * exp(R1 / m) - m
+    return(R2)
+}
