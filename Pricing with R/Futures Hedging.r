f5b1b4dad7ed4acd8be66f667935474bb1f9414b
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-02 10:17:04 +0800

    Update Futures Hedging.r

--- Pricing with R/Futures Hedging.r
+++ Pricing with R/Futures Hedging.r
@@ -1,3 +1,26 @@
+# For long side: $ return = (S_0 - S_t) + h(F_{t,T} - F_{0,T}) $
+dtttb = function(svar, fvar, cov, s0, st, f0, ft) {
+        h = cov/fvar
+        r = s0 - st + h*(ft - f0)
+        var = svar + h^2 * fvar - 2*h*cov
+        cat('Optimal long hedge ratio h:', h, '\n')
+        cat('Return of hedge:', r, '\n')
+        cat('Var of long hedge:', var, '\n')
+}
+
+ktttb = function(svar, fvar, cov, s0, st, f0, ft) {
+        h = cov/fvar
+        r = st - s0 - h*(ft - f0)
+        var = svar + h^2 * fvar - 2*h*cov
+        cat('Optimal short hedge ratio h:', h, '\n')
+        cat('Return of hedge:', r, '\n')
+        cat('Var of long hedge:', var, '\n')
+}
+
+
+
+
+
 
 
 
