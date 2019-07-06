725690d8590522230c7b9940a55a6b07f432d049
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-06 16:57:14 +0800

    Update Monte Carlo Simulation for Option Pricing.r

--- Pricing with R/Monte Carlo Simulation for Option Pricing.r
+++ Pricing with R/Monte Carlo Simulation for Option Pricing.r
@@ -1,3 +1,62 @@
+# 1-time simulation for S_T
+## Given Ito, $ S_T = S_t*exp((r - 0.5* sigma^2) + sigma * echo * sqrt(T-t)) $, echo is a normal distribution
+## sigma, rf are both yearly data
+sim_rand <- function(S, rf, sigma, t) {
+        R <- (rf - 0.5*sigma^2) * t
+        SD <- sigma * sqrt(t)
+        S_T <- 0.000000
+        S_T <- S * exp(R + SD*rnorm(1))
+        return(S_T)
+}
 
 
+
+
+# Monte Carlo Simulation for European Call
+payoff_call <- function(price, X) {
+        return(max(0, price - X))
+}
+
+p_sim <- function(S, r, sigma, t) {
+        R <- (r - 0.5*sigma^2)*t
+        SD <- sigma * sqrt(t)
+        return(S * exp(R + SD*rnorm(1)))
+}
+## Current price C_t for Call
+mc_eu_c <- function(S, X, r, sigma, t, n) {
+        s_t <- numeric(n)
+        sum_payoff <- numeric(n)
+        for (i in 1:n) {
+                s_t[i] <- p_sim(S, r, sigma, t)
+                sum_payoff[i] <- payoff_call(s_t[i], X)
+        }
+        s <- sum(sum_payoff)
+        return(exp(-r*t)*s/n)
+}
+
+
+
+
+
+# Monte Carlo Simulation for European Put
+payoff_put <- function(price, X) {
+        return(max(0, X- price))
+}
+
+p_sim <- function(S, r, sigma, t) {
+        R <- (r - 0.5*sigma^2)*t
+        SD <- sigma * sqrt(t)
+        return(S * exp(R + SD*rnorm(1)))
+}
+
+mc_eu_p <- function(S, X, r, sigma, t, n) {
+        s_t <- numeric(n)
+        sum_payoff <- numeric(n)
+        for (i in 1:n) {
+                s_t[i] <- p_sim(S, r, sigma, t)
+                sum_payoff[i] <- payoff_put(s_t[i], X)
+        }
+        s <- sum(sum_payoff)
+        return(exp(-r*t)*s/n)
+}
 
