d3f0b4c75a3a03dbf83369692e088cd3f56470c5
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-09 10:17:37 +0800

    Update Finite Difference Method for Option Pricing.r

--- Pricing with R/Finite Difference Method for Option Pricing.r
+++ Pricing with R/Finite Difference Method for Option Pricing.r
@@ -1,5 +1,6 @@
 # Explicit Finite Difference Method for European Call
-ef_eu_c <- function(S, X, r, sigma, t, price_step, t_step) {
+# $ f_{i,j} = a*f_{i+1,j-1} + b*f_{i+1,j} +c*f_{i+1,j+1} $
+ef_eu_c <- function(S, X, r, q, sigma, t, price_step, t_step) {
         sigma_square <- sigma^2
         M <- numeric(1)
         if ((price_step %% 2)==1) M <- price_step +1 else M <- price_step
@@ -13,17 +14,13 @@ ef_eu_c <- function(S, X, r, sigma, t, price_step, t_step) {
         a <- numeric(M)
         b <- numeric(M)
         c <- numeric(M)
-        r1 <- 1/(1 + r*delta_t)
-        r2 <- delta_t/(1 + r*delta_t)
         for (i in 2:M) {
-                a[i] <- r2*0.5*(i-1)*(-r+sigma_square*(i-1))
-                b[i] <- r1*(1-sigma_square*(i-1)^2 *delta_t)
-                c[i] <- r2*0.5*(i-1)*(r+sigma_square*(i-1))
+                a[i] <- -0.5*(r-q)*(i-1)*delta_t + 0.5*sigma_square*(i-1)^2*delta_t
+                b[i] <- 1-r*delta_t-sigma_square*(i-1)^2*delta_t
+                c[i] <- 0.5*(r-q)*(i-1)*delta_t + 0.5*sigma_square*(i-1)^2*delta_t
         }
         f_next <- numeric(M+1)
-        for(i in 1:(M+1)) {
-                f_next[i] <- max(0,S_values[i]-X)
-        }
+        f_next <- pmax(0, S_values - X)
         f <- numeric(M+1)
         for(j in N:1) {
                 f[1] <- 0
@@ -41,7 +38,7 @@ ef_eu_c <- function(S, X, r, sigma, t, price_step, t_step) {
 
 
 # Explicit Finite Difference Method for European Put
-ef_eu_p <- function(S, X, r, sigma, t, price_step, t_step) {
+ef_eu_p <- function(S, X, r, q, sigma, t, price_step, t_step) {
         sigma_square <- sigma^2
         M <- numeric(1)
         if ((price_step %% 2)==1) M <- price_step +1 else M <- price_step
@@ -55,17 +52,13 @@ ef_eu_p <- function(S, X, r, sigma, t, price_step, t_step) {
         a <- numeric(M)
         b <- numeric(M)
         c <- numeric(M)
-        r1 <- 1/(1 + r*delta_t)
-        r2 <- delta_t/(1 + r*delta_t)
         for (i in 2:M) {
-                a[i] <- r2*0.5*(i-1)*(-r+sigma_square*(i-1))
-                b[i] <- r1*(1-sigma_square*(i-1)^2 *delta_t)
-                c[i] <- r2*0.5*(i-1)*(r+sigma_square*(i-1))
+                a[i] <- -0.5*(r-q)*(i-1)*delta_t + 0.5*sigma_square*(i-1)^2*delta_t
+                b[i] <- 1-r*delta_t-sigma_square*(i-1)^2*delta_t
+                c[i] <- 0.5*(r-q)*(i-1)*delta_t + 0.5*sigma_square*(i-1)^2*delta_t
         }
         f_next <- numeric(M+1)
-        for(i in 1:(M+1)) {
-                f_next[i] <- max(0, X-S_values[i])
-        }
+        f_next <- pmax(0, X - S_values)
         f <- numeric(M+1)
         for(j in N:1) {
                 f[1] <- X
