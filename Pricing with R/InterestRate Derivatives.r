d8539b05d5f9e988db8f66cea2e6b186a75c220b
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-14 20:02:18 +0800

    Update InterestRate Derivatives.r

--- Pricing with R/InterestRate Derivatives.r
+++ Pricing with R/InterestRate Derivatives.r
@@ -41,11 +41,49 @@ bs_zbo_c <- function(B, X, R, sigma, t) {
 bs_zbo_p <- function(B, X, R, sigma, t) {
     sigmasqr <- sigma^2
     tsqr <- sqrt(t)
-    d1 <- (log(B/X) + (R + 0.5*sigmasqr*)*t) / (sigma * tsqr)
+    d1 <- (log(B/X) + (R +0.5*sigmasqr)*t) / (sigma*tsqr)
     d2 <- d1 - sigma*tsqr
     p <- X*exp(-R*t)*pnorm(-d2) - B*pnorm(-d1)
     return(p)
 }
+
+
+
+# Bond Call with coupon
+bs_cbo_c <- function(B, X, r, sigma, t, co_pay_times, co_amt) {
+    n <- t %/% co_pay_times
+    cou_amt_vector <- numeric(n-1)
+    cou_pay_times_vector <- numeric(n-1)
+    cou_discount <- numeric(n-1)
+    cou_amt_vector <- cou_amt_vector + co_amt
+    cou_pay_times_vector <- cou_pay_times_vector + co_pay_times
+    for (i in 1:(n-1)) {
+        cou_discount[i] <- cou_amt_vector[i] * exp(-r* i* cou_pay_times_vector[i])
+        }
+    total_discount <- sum(cou_discount)
+    adjust_B <- B - total_discount
+    return(bs_zbo_c(adjust_B, X, r, sigma, t))
+}
+
+
+
+# Bond Put with coupon
+bs_cbo_p <- function(B, X, r, sigma, t, co_pay_times, co_amt) {
+    n <- t %/% co_pay_times
+    cou_amt_vector <- numeric(n-1)
+    cou_pay_times_vector <- numeric(n-1)
+    cou_discount <- numeric(n-1)
+    cou_amt_vector <- cou_amt_vector + co_amt
+    cou_pay_times_vector <- cou_pay_times_vector + co_pay_times
+    for (i in 1:(n-1)) {
+        cou_discount[i] <- cou_amt_vector[i] * exp(-r* i* cou_pay_times_vector[i])
+        }
+    total_discount <- sum(cou_discount)
+    adjust_B <- B - total_discount
+    return(bs_zbo_p(adjust_B, X, r, sigma, t))
+}
+
+
 
 
 
