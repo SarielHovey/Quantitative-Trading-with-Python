ea94b5f5fc8c02a1e30fb520a2546a10951d7de0
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-20 21:24:36 +0800

    Update Greeks.r

--- Pricing with R/Greeks.r
+++ Pricing with R/Greeks.r
@@ -3,7 +3,7 @@ greek_call <- function(S, X, rf, sigma, T) {
         Ts <- sqrt(T)
         d1 <- (log(S/X) + (rf + 0.5*sigma^2)*T)/(sigma*sqrt(T))
         d2 <- d1 - sigma*Ts
-        nd1 <- 1/(sqrt(2*3.1415926)) * exp(-d1^2 /2)
+        nd1 <- 1/(sqrt(2*pi)) * exp(-d1^2 /2)
         Delta <- pnorm(d1)
         Gamma <- nd1 / (S*sigma*Ts)
         Theta <- -(S*sigma*nd1)/(2*Ts) - rf*X*exp(-rf*T)*pnorm(d2)
@@ -22,7 +22,7 @@ greek_put <- function(S, X, rf, sigma, T) {
         Ts <- sqrt(T)
         d1 <- (log(S/X) + (rf + 0.5*sigma^2)*T)/(sigma*sqrt(T))
         d2 <- d1 - sigma*Ts
-        nd1 <- 1/(sqrt(2*3.1415926)) * exp(-d1^2 /2)
+        nd1 <- 1/(sqrt(2*pi)) * exp(-d1^2 /2)
         Delta <- pnorm(d1) - 1
         Gamma <- nd1 / (S*sigma*Ts)
         Vega <- S * Ts * nd1
@@ -32,3 +32,37 @@ greek_put <- function(S, X, rf, sigma, T) {
         names(Greeks_P) <- c('Delta','Gamma','Theta','Vega','Rho')
         return(Greeks_P)
 }
+
+
+
+# Greeks for European Call(with div)
+## q is continuously compounded dividend yield
+## Delta_call for stocks with div is between 0 and exp(-q*T)
+## Gamma for Call or Put is always positive; Gamma max when option is At-the-Money
+greek_call <- function(S, X, rf, q, sigma, T) {
+    Ts <- sqrt(T)
+    d1 <- (log(S/X) + (rf - q + 0.5*sigma^2)*T)/(sigma*sqrt(T))
+    d2 <- d1 - sigma*Ts
+    nd1 <- 1/(sqrt(2*pi)) * exp(-d1^2 /2)
+    Delta <- exp(-q*T)*pnorm(d1)
+    Gamma <- exp(-q*T) * nd1 / (S*sigma*Ts)
+    Greeks_C <- c(Delta, Gamma)
+    names(Greeks_C) <- c('Delta','Gamma')
+    return(Greeks_C)
+}
+
+
+
+# Greeks for European Put(with div)
+greek_put <- function(S, X, rf, q, sigma, T) {
+    Ts <- sqrt(T)
+    d1 <- (log(S/X) + (rf - q + 0.5*sigma^2)*T)/(sigma*sqrt(T))
+    d2 <- d1 - sigma*Ts
+    Delta <- -exp(-q*T)*pnorm(-d1)
+    Gamma <- exp(-q*T) * nd1 / (S*sigma*Ts)
+    Greeks_P <- c(Delta, Gamma)
+    names(Greeks_P) <- c('Delta','Gamma')
+    return(Greeks_P)
+}
+
+
