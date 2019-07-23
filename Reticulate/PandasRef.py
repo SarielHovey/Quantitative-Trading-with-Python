485bf7197c84ab8c5ea26a6367c4e386c2dbc698
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-23 10:09:32 +0800

    Create PandasRef.py

---
+++ Reticulate/PandasRef.py
@@ -0,0 +1,8 @@
+#library(reticulate)        以下Python3代码均在R下的reticualte环境中运行,目的在于能够同时使用Python的数据清洗与R的数据分析
+#repl_python()      启动R下的Python解释器,输入exit则退出
+#>Python 3.7.3 (C:\PROGRA~3\ANACON~1\python.exe)
+#>Reticulate 1.12 REPL -- A Python interpreter in R.
+
+
+
+
