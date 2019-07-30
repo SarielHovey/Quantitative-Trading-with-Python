0bb98c04e48fce8b0b5c24b5ece5c153aac34108
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-30 10:13:58 +0800

    Add files via upload

---
+++ StrategyAndBacktest/07_CTA_TrendFollowing_MA.py
@@ -0,0 +1,5 @@
+# Trend Following Strategy should be used in time > 3 days to capture the trend in mkt
+# The strategy should be used when mkt shows obvious trend, NOT when mkt stays around a certain level
+# Remember: Futures mks is a gambel table. Usage of Kelly's Fuction is recommended
+# In this strategy, we use 2 MA lines, one Short term and one Long term to capture trends
+
