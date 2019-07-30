05d9c7ca59f846a18a5ff7492561deda0bb93dda
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-07-30 12:07:37 +0800

    Add files via upload
    Set stop loss or profit interval to -0.3 and 0.5
    Change MA to 3 and 5 days for better sensitivity

--- StrategyAndBacktest/07_CTA_TrendFollowing_MA.py
+++ StrategyAndBacktest/07_CTA_TrendFollowing_MA.py
@@ -2,7 +2,7 @@
 # The strategy should be used when mkt shows obvious trend, NOT when mkt stays around a certain level
 # Remember: Futures mks is a gambel table. Usage of Kelly's Fuction is recommended
 # In this strategy, we use 2 MA lines, one Short term and one Long term to capture trends
-
+# Warnning: The Strategy shows high profitability in past years, but behaves badly recently, either due to (1)limit of capacity; (2)tighter regulations
 import re
 import talib
 from datetime import datetime
@@ -38,9 +38,9 @@ def handle_data(context):
     current_short = futures_account.get_positions().get(symbol, dict()).get('short_amount', 0)
 
     history_data = context.history(symbol=symbol, attribute=['closePrice','openPrice','lowPrice','highPrice'], time_range=30, freq='1d')[symbol]
-    # Here we use MA5 and MA10
-    MA_S = talib.MA(history_data['closePrice'].apply(float).values,timeperiod=5)
-    MA_L = talib.MA(history_data['closePrice'].apply(float).values,timeperiod=10)
+    # Here we use MA3 and MA5
+    MA_S = talib.MA(history_data['closePrice'].apply(float).values,timeperiod=3)
+    MA_L = talib.MA(history_data['closePrice'].apply(float).values,timeperiod=5)
     if MA_S[-1] > MA_L[-1] and MA_S[-2] < MA_L[-2]:
         if current_short >0:
             futures_account.order(symbol, current_short, 'close')
@@ -62,14 +62,14 @@ def handle_data(context):
             futures_account.order(symbol, -current_long, 'close')
         if current_short > 0:
             futures_account.order(symbol, current_short, 'close')
-        print(datetime.strftime(context.current_date, '%Y%m%d') + ' 止损')
+        print(datetime.strftime(context.current_date, '%Y-%m-%d') + ' 止损')
 
     if margin and profit/margin > 0.05:
         if current_long >0:
             futures_account.order(symbol, -current_long, 'close')
         if current_short > 0:
             futures_account.order(symbol, current_short, 'close')
-        print(datetime.strftime(context.current_date, '%Y%m%d') + ' 止盈')   
+        print(datetime.strftime(context.current_date, '%Y-%m-%d') + ' 止盈')   
 
 def main_contract_mapping_changed(context, futures_account):
     if context.mapping_changed('RBM0'):
