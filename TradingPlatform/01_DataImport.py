7e1cc6e1b27418d2d7805b89f4716394c45e7a8c
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-08-03 11:15:12 +0800

    Update 01_DataImport.py

--- TradingPlatform/01_DataImport.py
+++ TradingPlatform/01_DataImport.py
@@ -1,3 +1,14 @@
-# Asscess mkt data from Quandl API
+# Asscess mkt data from tuShare Pro
+import tushare as ts
+print(ts.__version__)
+#>1.2.39
+
+# Set token here, only needed for the 1st time
+ts.set_token('Token Value')
+# Use pro API
+pro = ts.pro_api()
+
+
+
 
 
