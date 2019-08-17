7e47d7836dc570290fd3c6d71d411825e0d7939d
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-08-17 13:14:37 +0800

    Update SimpleMAModel.r

--- TimeSeriesAnalysis/SimpleMAModel.r
+++ TimeSeriesAnalysis/SimpleMAModel.r
@@ -1,3 +1,5 @@
-
-
+# There is no ma(x) in R as ar(x). But we could set order in arima(x)
+# order=c(0,0,3) means a simple MA(3) model
+x.ma <- arima(x, order=c(0, 0, 3))
+# It is suggested to use ARIMA instead of simple AR or MA model
 
