2f3e7fafbdc5639e84a27824a770b1a8566775ac
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-09-01 17:44:32 +0800

    Update HurstExponent.py

--- TimeSeriesAnalysis/HurstExponent.py
+++ TimeSeriesAnalysis/HurstExponent.py
@@ -1,3 +1,26 @@
+from __future__ import print_function
+import statsmodels.tsa.stattools as ts
+from datetime import datetime
+from numpy import cumsum, log, polyfit, sqrt, std, subtract
+from numpy.random import randn
 
+def hurst(ts):
+    """ Return Hurst Exponent of time series vector ts"""
+    lags = range(2, 100)
+    tau = [sqrt(std(subtract(ts[lag:], ts[:-lag]))) for lag in lags]
+    poly = polyfit(log(lags), log(tau), 1)
+    return poly[0] * 2.0
+  
+gbm = log(cumsum(randn(100000)) + 1000)
+mr = log(randn(100000) + 1000)
+tr = log(cumsum(randn(100000)+1) + 1000)
 
+print('Hurst for Geometric Brownian Motion  %s' % hurst(gbm))
+print('Hurst for Mean-Reverting %s' % hurst(mr))
+print('Hurst for Trending Series %s' % hurst(tr))
+#>Hurst for Geometric Brownian Motion  0.5094958688977529
+#>Hurst for Mean-Reverting 6.374652781798867e-05
+#>Hurst for Trending Series 0.9520874471335533
+
+# Now let's try some real world data
 
