9fcf6db2e84eb3543be485ba5991ac3adcb9aaed
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-09-21 22:47:16 +0800

    Add files via upload

--- C++/Pricing/EuropeanOption/EuropeanOption.hpp
+++ C++/Pricing/EuropeanOption/EuropeanOption.hpp
@@ -1,7 +1,49 @@
-#include <iostream>;
-#include <cmath>;
+#include <iostream>
+#include <string>
+class EuropeanOption
+{
+private:
+    void init();
+    void copy(const EuropeanOption& o2);
+    /* Kernel Functions for price and Greeks */
+    double CallPrice() const;
+    double PutPrice() const;
+    double CallDelta() const;
+    double PutDelta() const;
+    double CallGamma() const;
+    double PutGamma() const;
+    double CallVega() const;
+    double PutVega() const;
+    double CallTheta() const;
+    double PutTheta() const;
+    double CallRho() const;
+    double PutRho() const;
+    /* data */
+public:
+    double r;
+    double sigma;
+    double K;
+    double T;
+    double S0;
+    double b;
+    string optType;
 
+public:
+EuropeanOption();
+EuropeanOption(const EuropeanOption& option2);
+EuropeanOption(const string& optionType);
 
+virtual ~EuropeanOption();
 
+EuropeanOption& operator = (const EuropeanOption& option2);
+double Price() const;
+double Delta() const;
+double Gamma() const;
+double Vega() const;
+double Theta() const;
+double Rho() const;
 
+void toggle();
+
+};
 
