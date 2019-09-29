5606689e7cf14866a357f39da42a992c232ebd29
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-09-29 17:14:32 +0800

    Update FunctionPointer.cpp

--- C++/Closet/FunctionPointer.cpp
+++ C++/Closet/FunctionPointer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-
-
+/* (*f) here is a Function Pointer.
+for genericFunction, as long as myX, myY are all double, then it will work.    */
 void genericFunction (double myX, double myY, double (*f) (double x, double y))
 {
 // Call the function f with arguments myX and myY
@@ -28,6 +28,7 @@ return x - y;
 int main() {
     double x = 3;
     double y = 4;
+    // Below is the example when using Function Pointer. We put function on the place of (*f)
     genericFunction(x, y, add);
     genericFunction(x, y, multiply);
     genericFunction(x, y, subtract);
