dedda60b8e173107888aaef4ac8cc11861c8c1e4
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-10-19 13:15:01 +0800

    Update PointerOperatorAndAdressOperator.cpp

--- C++/Closet/PointerOperatorAndAdressOperator.cpp
+++ C++/Closet/PointerOperatorAndAdressOperator.cpp
@@ -30,3 +30,25 @@ int main()
     return 0;
 }
 
+
+/*
+Showing that * and & are inverses of each other.
+a= 7
+&a= 0x7ffff425062c
+*a is not valid as a is an int not pointer.
+aPtr= 0x7ffff425062c
+&aPtr= 0x7ffff4250630
+*aPtr= 7
+&*a is not valid as a is an int not pointer.
+*&a= 7
+&*aPtr = 0x7ffff425062c
+*&aPtr = 0x7ffff425062c
+&*&a= 0x7ffff425062c
+*&*aPtr= 7
+&*&aPtr= 0x7ffff4250630
+Conclusion:
+(1)&a = aPtr, is the Memory Position of a
+(2)a = *aPtr, is the value Memory Position of a points to
+(3)&*aPtr = *&aPtr = aPtr = &*&a, because & and * are inverses
+(4)&aPtr is the Memory Position of aPtr, which points to &a    
+*/
