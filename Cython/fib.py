0b481af324d251f48647d954fa765cf306d32d92
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-10-29 21:24:20 +0800

    Update fib.py

--- Cython/fib.py
+++ Cython/fib.py
@@ -3,7 +3,7 @@
 # Caution: cythonmagic has been moved to cython package, not in ipython anymore, so %load_ext cythonmagic will not work
 %%cython
 def fib(int n):
-    cdef int n
+    cdef int i
     cdef double a=0.0, b= 1.0
     for i in range(n):
         a, b = a+b, a
