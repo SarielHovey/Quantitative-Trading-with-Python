f473da9e7dd37e16d046d613bfd97162c9d1bc0e
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-11-10 23:16:39 +0800

    Add files via upload

--- Python/MathematicalTools.py
+++ Python/MathematicalTools.py
@@ -35,4 +35,33 @@ def op_eu_mcs(M, I, T, S0, K, r, sigma, option='C'):
                 print("Error: option should be 'C' or 'P'. ")
                 return 'Error!'
 
+
+def op_am_mcs(M, I, T, S0, K, r, sigma, option='C'):
+        '''
+        Monte Carlo Simulation for American Option based on standard BSM by Least-squares regression algorithm
+        '''
+        dt = T/M
+        df = np.exp(-r*dt)
+        S = np.zeros((M+1, I))
+        S[0] = S0
+        sn = gen_sn(M, I)
+        for t in range(1, M+1):
+                S[t] = S[t-1] * np.exp((r - 0.5*sigma**2)*dt + sigma*np.sqrt(dt)*sn[t])
+        if option == 'C':
+                h = np.maximum(S[-1]-K, 0)
+        elif option == 'P':
+                h = np.maximum(K-S[-1], 0)
+        else:
+                print("Error: option should be 'C' or 'P'. ")
+                return 'Error!'
+        V = np.copy(h)
+        for t in range(M-1, 0, -1):
+                reg = np.polyfit(S[t], V[t+1], 7)
+                C = np.polyval(reg, S[t])
+                V[t] = np.where(C>h[t], V[t+1]*df, h[t])
+        return df * np.mean(V[1])
+
+
+
+
         
