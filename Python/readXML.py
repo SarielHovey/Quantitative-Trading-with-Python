31a00da3fbc705870f6daee06b57067e103c63ce
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-12-13 11:07:07 +0800

    Update readXML.py

--- Python/readXML.py
+++ Python/readXML.py
@@ -9,7 +9,7 @@ nsList = {'ns0':'subClass:a','ns1':'subClass:b'}
 root.xpath('./class1/class2/ns0:class1/ns1:class2/tag1', namespaces=nsList)[0].tag
 root.xpath('./class1/class2/ns0:class1/ns1:class2/tag1', namespaces=nsList)[0].text
 root.xpath('./class1/class2/ns0:class1/ns1:class2/tag1', namespaces=nsList)[0].attrib
-
+root.xpath('./class1/class2/ns0:class1/ns1:class2/tag1[@attrib="Attrib1"]', namespaces=nsList)[0].text
 
 
 
