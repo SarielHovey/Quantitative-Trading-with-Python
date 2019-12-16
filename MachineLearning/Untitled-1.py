9e6159808df1364201c5734aa99a735ac665db0d
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-12-16 15:37:09 +0800

    Add files via upload

--- MachineLearning/Untitled-1.py
+++ MachineLearning/Untitled-1.py
@@ -3,4 +3,22 @@ from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', version=1)
 from sklearn.externals import joblib
 joblib.dump(mnist, 'MNIST.pkl')
+mnist = joblib.load('MNIST.pkl')
+
+import numpy as np
+y = y.astype(int)
+X, y = mnist['data'], mnist['target']
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test ==5)
+'''
+array([ True, False, False, ...,  True, False, False])
+'''
+
+# Use a Stochastic Gradient Descent classifier to run a naive classification
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=43)
+sgd_clf.fit(X_train, y_train_5)
+
+
 
