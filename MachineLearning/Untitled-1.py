ffb9070afb436ac13697cce5cb8d88ad68c53be2
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2019-12-22 13:47:15 +0800

    Add files via upload

--- MachineLearning/Untitled-1.py
+++ MachineLearning/Untitled-1.py
@@ -121,3 +121,27 @@ Result: $y = 1.9845 + 2.9922 * X + 0.5096 * X^2$
 Underlying real model: $y = 2 + norm(0,1) + 3X + 0.5X^2$
 (array([1.98446473]), array([[2.99220428, 0.5096327 ]]))
 '''
+
+## Learning curve for Overfitting detection
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+### Change list to np.array and use Numba if performance preferred
+def plot_learning_curve(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=.2)
+    train_error, val_error = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_pred = model.predict(X_train[:m])
+        y_val_pred = model.predict(X_val)
+        train_error.append(mean_squared_error(y_train[:m], y_train_pred))
+        val_error.append(mean_squared_error(y_val, y_val_pred))
+    plt.plot(np.sqrt(train_error),'r-+',linewidth=2,label='train')
+    plt.plot(np.sqrt(val_error),'b-',linewidth=3,label='val')
+    plt.xlabel('Training Set Size')
+    plt.ylabel('RMSE')
+    plt.legend(loc=0)
+    #plt.show()
+'''
+
+'''
