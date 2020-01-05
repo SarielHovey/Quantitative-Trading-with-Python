0fd8ce32c7f34cbf414fb9ff19ac40d684d3dcd9
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-01-05 21:03:00 +0800

    Add files via upload

--- MachineLearning/Untitled-1.py
+++ MachineLearning/Untitled-1.py
@@ -11,7 +11,7 @@ from sklearn.svm import SVC
 
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_moons
-X, y = make_moons(n_samples=5000, noise=0.15)
+X, y = make_moons(n_samples=500, noise=0.15)
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=.2)
 
 log_clf = LogisticRegression()
@@ -61,6 +61,7 @@ Use the same training algorithm on different random subsets of the training set
 Bootstrap aggregating: sample with replacement 有放回
 Pasting: sample without replacement 无放回
 Prediction is made by aggregating the predictions of all predictors
+e.g. below is an ensemble of 500 Decision Trees
 This model could be parallel run
 '''
 from sklearn.ensemble import BaggingClassifier
@@ -69,9 +70,74 @@ bag_clf = BaggingClassifier(
     DecisionTreeClassifier(), n_estimators=500,
     max_samples=100, bootstrap=True,n_jobs=-1
 ) # automaticly use soft vote if algo used has predict_probab() method
+# n_jobs is the number of cpu cores to use. -1 means all
 bag_clf.fit(X_train,y_train)
 y_pred = bag_clf.predict(X_val)
 print(bag_clf.__class__.__name__, accuracy_score(y_val, y_pred))
 '''
-BaggingClassifier 0.979
+BaggingClassifier 0.974
 '''
+pas_clf = BaggingClassifier(
+    DecisionTreeClassifier(), n_estimators=500,
+    max_samples=100, bootstrap=False,n_jobs=-1
+)
+pas_clf.fit(X_train,y_train)
+y_pred = pas_clf.predict(X_val)
+print('PassingClassifier ', accuracy_score(y_val, y_pred))
+'''
+PassingClassifier 0.976
+'''
+
+
+## Out-of-Bag Evaluation
+'''
+With bagging, some instances in training set may never be chosen for training
+Thus they could be used for model evaluation
+'''
+bag_clf = BaggingClassifier(
+    DecisionTreeClassifier(), n_estimators=500,
+    max_samples=100, bootstrap=True,n_jobs=-1,oob_score=True
+)
+bag_clf.fit(X_train,y_train)
+bag_clf.oob_score_
+'''
+Out-of-Bag evaluation: 0.975
+'''
+y_pred = bag_clf.predict(X_val)
+accuracy_score(y_val, y_pred)
+'''
+BaggingClassifier: 0.976
+'''
+bag_clf.oob_decision_function_
+'''
+class probability for each training instance
+shape: (4000,2) # 5000*.8=4000
+array([[1.        , 0.        ],
+       [0.05590062, 0.94409938],
+       [0.01629328, 0.98370672],
+       ...,
+       [0.9958159 , 0.0041841 ],
+       [1.        , 0.        ],
+       [0.01229508, 0.98770492]])
+'''
+
+
+
+# Random Forest
+'''
+Random Forest is an ensemble of Decision Trees, with max_samples = size of training set
+'''
+from sklearn.ensemble import RandomForestClassifier
+rnd_clf = RandomForestClassifier(n_estimators=500,max_leaf_nodes=16,n_jobs=-1)
+rnd_clf.fit(X_train,y_train)
+y_pred = rnd_clf.predict(X_val)
+accuracy_score(y_val, y_pred)
+'''
+Random Forest Classifier: 0.986
+the same could be achieved by bag_clf
+bag_clf = BaggingClassifier(
+    DecisionTreeCalssifier(splitter='random',max_leaf_nodes=16),
+    n_estimators=500,max_samples=1.0,bootstrap=True,n_jobs=-1
+)
+'''
+
