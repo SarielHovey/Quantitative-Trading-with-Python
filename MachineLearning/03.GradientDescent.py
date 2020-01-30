3d30b0a2fdc017f4c52a9fb2354eb24ee3b2c660
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-01-30 20:47:15 +0800

    Update 03.GradientDescent.py

--- MachineLearning/03.GradientDescent.py
+++ MachineLearning/03.GradientDescent.py
@@ -143,10 +143,30 @@ def squared_loss(y_hat,y):
 def sgd(params,lr,batch_size):
     '''
     Perform Gradient Descent on "params"
+    [:] sets inplace change
     '''
     for param in params:
         param[:] = param - lr * param.grad / batch_size
 
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+batch_size = 10
+for epoch in range(num_epochs):
+    for X, y in data_iter(batch_size, features, labels):
+        with autograd.record():
+            l = loss(net(X,w,b),y)
+        l.backward()
+        sgd([w,b],lr,batch_size)
+    train_l = loss(net(features,w,b),labels)
+    print('epoch %d, loss %f' % (epoch +1, train_l.mean().asnumpy()))
+    
+'''
+epoch 1, loss 0.016662
+epoch 2, loss 0.000077
+epoch 3, loss 0.000050
+'''
 
 
 
