c3c3736bcd70d8c2431a2129d02dd158ceb15ff1
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-02-07 17:41:25 +0800

    Update retrieving_data.py

--- qs_sat/retrieving_data.py
+++ qs_sat/retrieving_data.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
     # Connect to the MySQL instance
     DB_HOST = 'localhost'
     DB_USER = 'sec_user'
-    DB_PASS = 'password'
+    DB_PASS = 'shuangshuang'
     DB_NAME = 'securities_master'
     con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
     
-    # Select all of the historic Google adjusted close data
     sql = "SELECT dp.price_date, dp.close_price, dp.adj_factor FROM symbol AS sym INNER JOIN daily_price AS dp ON dp.symbol_id = sym.id WHERE sym.ticker = '601988' ORDER BY dp.price_date ASC;"
+    sql2 = "SELECT sy.ticker, dl.price_date, dl.close_price, dl.adj_factor, dl.volume FROM daily_price AS dl INNER JOIN symbol as sy WHERE sy.ticker='601988' ORDER BY dl.price_date DESC;"
 
     # Create a pandas dataframe from the SQL query
-    ZGYH = pd.read_sql_query(sql, con=con, index_col='price_date')
+    ZGYH = pd.read_sql_query(sql2, con=con, index_col='price_date')
     # Output the dataframe tail
     print(ZGYH.tail())
