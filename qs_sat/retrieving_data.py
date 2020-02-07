a67295a15c23ecc58c5f992cbbebabb5723f788c
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-02-07 13:56:13 +0800

    Update retrieving_data.py

--- qs_sat/retrieving_data.py
+++ qs_sat/retrieving_data.py
@@ -10,16 +10,9 @@ if __name__ == "__main__":
     con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
     
     # Select all of the historic Google adjusted close data
-    sql = """
-    SELECT dp.price_date, dp.adj_close_price
-    FROM symbol AS sym 
-    INNER JOIN daily_price AS dp 
-    ON dp.symbol_id = sym.id 
-    WHERE sym.ticker = 'GOOG' 
-    ORDER BY dp.price_date ASC;
-    """
+    sql = "SELECT dp.price_date, dp.close_price, dp.adj_factor FROM symbol AS sym INNER JOIN daily_price AS dp ON dp.symbol_id = sym.id WHERE sym.ticker = '601988' ORDER BY dp.price_date ASC;"
 
     # Create a pandas dataframe from the SQL query
-    goog = pd.read_sql_query(sql, con=con, index_col='price_date')
+    ZGYH = pd.read_sql_query(sql, con=con, index_col='price_date')
     # Output the dataframe tail
-    print(goog.tail())
+    print(ZGYH.tail())
