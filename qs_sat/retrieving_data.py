97d74c58c62232fdf2222c8d38b575cf75d6b2c0
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-02-11 20:38:20 +0800

    Update retrieving_data.py

--- qs_sat/retrieving_data.py
+++ qs_sat/retrieving_data.py
@@ -12,13 +12,16 @@ if __name__ == "__main__":
     sql = """
         SELECT dp.price_date, dp.close_price, dp.adj_factor 
         FROM symbol AS sym 
-        INNER JOIN daily_price AS dp ON dp.symbol_id = sym.id 
+        INNER JOIN daily_price AS dp 
+        ON dp.symbol_id = sym.id 
         WHERE sym.ticker = '601988' 
         ORDER BY dp.price_date ASC;
         """
     sql2 = """
         SELECT sy.ticker, dl.price_date, dl.close_price, dl.adj_factor, dl.volume 
-        FROM daily_price AS dl INNER JOIN symbol as sy 
+        FROM daily_price AS dl 
+        INNER JOIN symbol as sy 
+        ON dl.symbol_id = sy.id 
         WHERE sy.ticker='601988' 
         ORDER BY dl.price_date DESC;
         """
@@ -27,3 +30,6 @@ if __name__ == "__main__":
     ZGYH = pd.read_sql_query(sql2, con=con, index_col='price_date')
     # Output the dataframe tail
     print(ZGYH.tail())
+
+    
+    
