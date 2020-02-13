a54bd1e54afb36ab08c0bb988a391941c4d89d00
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-02-13 13:28:46 +0800

    Add support to sql DATABASE to class

--- qs_sat/pricing/tushare.py
+++ qs_sat/pricing/tushare.py
@@ -1,9 +1,10 @@
 from datetime import datetime as dt
 import pandas as pd
-import tushare as ts
+import tushare as tu
+import MySQLdb as mdb
 
 
-COLUMNS = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
+COLUMNS = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'AdjFacotr']
 class TuShare(object):
     """
     Encapsulates calls to the TuShare API with a provided API key.
@@ -40,13 +41,12 @@ class TuShare(object):
             ticker += '.SZ'
         return ticker
 
-    def get_daily_historic_data(self, ts_code, adj, startdate, enddate):
+    def get_daily_historic_data(self, ts_code, adj, start_date, end_date):
         """
-        Use the generated API call to query AlphaVantage with the appropriate API key and return a list of price tuples for particular ticker.
+        Use the generated API call to query Tushare with the appropriate API key and return a list of price tuples for particular ticker.
         Parameters
         ----------
-        ticker : 'str', The ticker symbol, e.g. '600388.SZ'
-        adj : Default as 'qfq'
+        ts_code : 'str', The ticker symbol in tushare format, e.g. '600388.SZ'
         start_date : 'datetime', The starting date to obtain pricing for
         end_date : 'datetime', The ending date to obtain pricing for
         Returns
@@ -55,10 +55,36 @@ class TuShare(object):
         """
         ticker = self._construct_tushare_symbol_call(ts_code)
         try:
-            ts_data_df = ts.pro_bar(ts_code=ticker, adj='qfq', start_date=startdate, end_date=enddate, pro_api=self.api_key)
+            data0 = tu.pro_bar(ts_code=ts_code, start_date=start_date, end_date=end_date,adj=None)
+            data1 = tu.pro_api().adj_factor(ts_code=ts_code, start_date=start_date, end_date=end_date)
         except Exception as e:
             print("Could not download Tushare data for %s ticker (%s)...stopping." % (ticker, e))
             return pd.DataFrame(columns=COLUMNS)
         else:
-            ts_data_df['trade_date'] = pd.to_datetime(ts_data_df['trade_date'])
-            return ts_data_df.set_index('trade_date')
+            data = pd.merge(data0[['ts_code','trade_date','open','high','low','close','vol']],data1[['trade_date','adj_factor']],how='left',left_on='trade_date',right_on='trade_date')
+            data.vol = data.vol.apply(lambda x: int(x * 100))
+            data.trade_date = pd.to_datetime(data.trade_date)
+            return data.set_index('trade_date')
+
+    def get_daily_data_sql(self, ticker, startdate, enddate):
+        """
+        Use DATABASE securities_master to query data for ticker input.
+        Parameters
+        ----------
+        ticker : 'str', The ticker symbol, e.g. '601988'
+        start_date : str, '%Y-%m-%d %H:%M:%S', The starting date to obtain pricing for
+        end_date : str, '%Y-%m-%d %H:%M:%S', The ending date to obtain pricing for
+        Returns
+        -------
+        'pd.DataFrame', The frame of OHLCV prices and volumes
+        """
+        DB_HOST = 'localhost'
+        DB_USER = 'sec_user'
+        DB_PASS = 'YOUR_PASSWORD'
+        DB_NAME = 'securities_master'
+        con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+        sql = "SELECT sym.ticker, dp.price_date, dp.open_price, dp.high_price, dp.low_price, dp.close_price, dp.adj_factor, dp.volume FROM daily_price AS dp INNER JOIN symbol AS sym ON sym.id = dp.symbol_id where dp.price_date between ' " + startdate + "' AND '" + enddate + "' ORDER BY dp.price_date ASC;"
+        otpt = pd.read_sql_query(sql, con=con, index_col='price_date')
+        return otpt
+
+        
