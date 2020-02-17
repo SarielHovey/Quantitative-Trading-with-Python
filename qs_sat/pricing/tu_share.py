106ab739bc5d45302cd8d115868802cf309139cb
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-02-17 19:53:34 +0800

    Update and rename tushare.py to tu_share.py
    Update get_daily_historic_data() method for funds, futures support.

--- qs_sat/pricing/tu_share.py
+++ qs_sat/pricing/tu_share.py
@@ -35,27 +35,33 @@ class TuShare(object):
         """
         if ticker[:2] == '60':
             ticker += '.SH'
+        elif ticker[:1] == '5':
+            ticker += '.SH'
         elif ticker[:2] == '00':
             ticker += '.SZ'
         elif ticker[:2] == '30':
             ticker += '.SZ'
+        elif ticker[:1] == '1':
+            ticker += '.SZ'
         return ticker
 
-    def get_daily_historic_data(self, ts_code, adj, start_date, end_date):
+    def get_daily_historic_data(self, ticker, start_date, end_date, asset='E', adj=None):
         """
         Use the generated API call to query Tushare with the appropriate API key and return a list of price tuples for particular ticker.
         Parameters
         ----------
-        ts_code : 'str', The ticker symbol in tushare format, e.g. '600388.SZ'
+        ticker : 'str', The ticker symbol, e.g. '600388'
         start_date : 'datetime', The starting date to obtain pricing for
         end_date : 'datetime', The ending date to obtain pricing for
+        asset: 'str', E--Equity, FD--Fund, FT--Future
         Returns
         -------
         'pd.DataFrame', The frame of OHLCV prices and volumes
         """
-        ticker = self._construct_tushare_symbol_call(ts_code)
+        ts_code = self._construct_tushare_symbol_call(ticker)
+        # print(ticker, ts_code) # Used for Debug
         try:
-            data0 = tu.pro_bar(ts_code=ts_code, start_date=start_date, end_date=end_date,adj=None)
+            data0 = tu.pro_bar(ts_code=ts_code, start_date=start_date, end_date=end_date, asset=asset, adj=adj)
             data1 = tu.pro_api().adj_factor(ts_code=ts_code, start_date=start_date, end_date=end_date)
         except Exception as e:
             print("Could not download Tushare data for %s ticker (%s)...stopping." % (ticker, e))
@@ -64,7 +70,8 @@ class TuShare(object):
             data = pd.merge(data0[['ts_code','trade_date','open','high','low','close','vol']],data1[['trade_date','adj_factor']],how='left',left_on='trade_date',right_on='trade_date')
             data.vol = data.vol.apply(lambda x: int(x * 100))
             data.trade_date = pd.to_datetime(data.trade_date)
-            return data.set_index('trade_date')
+            data.adj_factor.fillna(1.0,inplace=True)
+            return data.set_index('trade_date').sort_index()
 
     def get_daily_data_sql(self, ticker, startdate, enddate):
         """
@@ -80,11 +87,9 @@ class TuShare(object):
         """
         DB_HOST = 'localhost'
         DB_USER = 'sec_user'
-        DB_PASS = 'Your_Password_Here'
+        DB_PASS = 'shuangshuang'
         DB_NAME = 'securities_master'
         con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
         sql = "SELECT sym.ticker, dp.price_date, dp.open_price, dp.high_price, dp.low_price, dp.close_price, dp.adj_factor, dp.volume FROM daily_price AS dp INNER JOIN symbol AS sym ON sym.id = dp.symbol_id where " + "sym.ticker = '" + ticker + "' AND " + "dp.price_date BETWEEN ' " + startdate + "' AND '" + enddate + "' ORDER BY dp.price_date ASC;"
         otpt = pd.read_sql_query(sql, con=con, index_col='price_date')
         return otpt
-
-        
