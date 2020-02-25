38bdb79fe6fa8f73d5370c485ad9b1b161b88885
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-02-25 16:47:30 +0800

    Test Passed.
    Sample for typical AlphaVantage output also attached.

--- qs_sat/qs_code/execution.py
+++ qs_sat/qs_code/execution.py
@@ -24,29 +24,29 @@ class ExecutionHandler(object):
         """
         raise NotImplementedError("Should implement execute_order()")
 
-    class SimulatedExecutionHandler(ExecutionHandler):
+class SimulatedExecutionHandler(ExecutionHandler):
+    """
+    The simulated execution handler simply converts all order objects into their equivalent fill objects automatically without latency, slippage or fill-ratio issues.
+    This allows a straightforward "first go" test of any strategy, before implementation with a more sophisticated execution handler.
+    """
+    def __init__(self, events):
         """
-        The simulated execution handler simply converts all order objects into their equivalent fill objects automatically without latency, slippage or fill-ratio issues.
-        This allows a straightforward "first go" test of any strategy, before implementation with a more sophisticated execution handler.
+        Initialises the handler, setting the event queues up internally.
+
+        Parameters:
+        events - The Queue of Event objects.
         """
-        def __init__(self, events):
-            """
-            Initialises the handler, setting the event queues up internally.
+        self.events = events
 
-            Parameters:
-            events - The Queue of Event objects.
-            """
-            self.events = events
+    def execute_order(self, event):
+        """
+        Simply converts Order objects into Fill objects naively, i.e. without any latency, slippage or fill ratio problems.
 
-        def execute_order(self, event):
-            """
-            Simply converts Order objects into Fill objects naively, i.e. without any latency, slippage or fill ratio problems.
-
-            Parameters:
-            event - Contains an Event object with order information.
-            """
-            if event.type == 'ORDER':
-                fill_event = FillEvent( datetime.datetime.utcnow(), event.symbol, 'ARCA', event.quantity, event.direction, None)
-                self.events.put(fill_event)
+        Parameters:
+        event - Contains an Event object with order information.
+        """
+        if event.type == 'ORDER':
+            fill_event = FillEvent( datetime.datetime.utcnow(), event.symbol, 'ARCA', event.quantity, event.direction, None)
+            self.events.put(fill_event)
 
             
