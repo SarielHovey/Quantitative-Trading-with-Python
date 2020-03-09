2a3a713ed7e3d7c64b6690c1a68bc97432a3cee4
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-03-09 18:18:36 +0800

    Add files via upload

--- qs_sat/advanced/rpy2_example.py
+++ qs_sat/advanced/rpy2_example.py
@@ -1,5 +1,6 @@
 from rpy2 import robjects
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
@@ -55,6 +56,37 @@ setwd("D:/Quant/qs_sat/advance/")
 jpeg(file='Rplot.jpg')
 w <- rep(1:10, 10)
 acf(w)
+dev.off()
+"""
+r(str)
+
+# Random Walk ACF
+str = """
+x <- w <- rnorm(1000)
+for (t in 2:1000) {x[t] <- x[t-1] + w[t]}
+setwd("D:/Quant/qs_sat/advance/")
+jpeg(file='Rplot.jpg')
+acf(x)
+dev.off()
+"""
+r(str)
+str = """
+setwd("D:/Quant/qs_sat/advance/")
+jpeg(file='Rplot.jpg')
+acf(diff(x))
+dev.off()
+"""
+r(str)
+
+# Reality Stock ACF
+r("ubs <- read.csv('UBS.csv',sep=',',header=TRUE)")
+list(r('colnames(ubs)'))
+#['Date', 'Open', 'High', 'Low', 'Close', 'Adj.Close', 'Volume']
+r("names(ubs) <- c('Date','Op','Hi','Lo','Cl','Ad','Vo')")
+str = """
+setwd("D:/Quant/qs_sat/advance/")
+jpeg(file='Rplot.jpg')
+acf(diff(ubs$Ad), na.action=na.omit)
 dev.off()
 """
 r(str)
