51018191e7f3e909f7ecd4e41f54ba0cf491a035
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-03-12 18:45:08 +0800

    Add files via upload

--- qs_sat/advanced/ryp2_ARIMA-GARCH.py
+++ qs_sat/advanced/ryp2_ARIMA-GARCH.py
@@ -196,3 +196,70 @@ r(str)
 #>
 #>data:  final.arma$resid
 #>X-squared = 19.485, df = 20, p-value = 0.4905
+
+
+# GARCH(1,1)
+"""
+$\epsilon_t = \sigma_t \omega_t$
+$\sigma_t^2 = \alpha_0 + \alpha_1 \epsilon_{t-1}^2 + \beta_1 \sigma_{t-1}^2$
+with $\alpha_1 + \beta_1 < 1$ and $E(\epsilon)=0$
+"""
+str = """
+a0 <- 0.2
+a1 <- 0.5
+b1 <- 0.3
+w <- rnorm(10000)
+eps <- rep(0, 10000)
+sigsq <- rep(0, 10000)
+for (i in 2:10000) {
+    sigsq[i] <- a0 + a1 * (eps[i-1]^2) + b1 * sigsq[i-1]
+    eps[i] <- w[i]*sqrt(sigsq[i])
+}
+acf(eps)
+acf(eps^2)
+"""
+r(str)
+r('library(tseries)')
+r('eps.garch <- garch(eps, trace=FALSE)')
+r('confint(eps.garch)')
+#>          2.5 %     97.5 %
+#> a0  0.83936601 0.93098694
+#> a1  0.19523413 0.22970212
+#> b1 -0.01530324 0.01530324
+
+
+# GARCH with Financial Data
+str = """
+ubs <- read.csv('UBS.csv',sep=',',header=TRUE)
+names(ubs) <- c('Date','Op','Hi','Lo','Cl','Ad','Vo')
+ubsrt <- diff(log(ubs$Cl))
+final.aic <- Inf
+final.order <- c(0,0,0)
+for (i in 0:4) for (j in 0:2) for(k in 0:4) {
+    current.aic <- AIC(arima(ubsrt, order=c(i, j, k)))
+    if (current.aic < final.aic) {
+        final.aic <- current.aic
+        final.order <- c(i, j, k)
+        final.arma <- arima(ubsrt, order=final.order)
+        }}
+"""
+r(str)
+r('arima(x = ubsrt, order = final.order)')
+#> 
+#> Coefficients:
+#>          ar1      ar2      ar3      ar4      ma1     ma2     ma3  intercept
+#>       0.0063  -0.2509  -0.8302  -0.0481  -0.0899  0.2685  0.7791     -2e-04
+#> s.e.  0.1781   0.1242   0.1452   0.0354   0.1761  0.1477  0.1637      4e-04
+#> 
+#> sigma^2 estimated as 0.0004227:  log likelihood = 6303.99,  aic = -12589.99
+r('acf(final.arma$resid)') # No sign of obvious conditional heteroskedasticity
+r('acf(final.arma$resid^2)') # Obvious conditional heteroskedasticity
+r('ubsrt.garch <- garch(ubsrt)')
+r('ubsrt.res <- ubsrt.garch$resid[-1]') # 1st element is NA
+r('confint(ubsrt.garch)')
+#>           2.5 %       97.5 %
+#> a0 5.899139e-06 0.0000112429
+#> a1 3.740404e-02 0.0500900303
+#> b1 9.264154e-01 0.9478731433
+r('acf(ubsrt.res)')
+r('acf(ubsrt.res^2)') # No sign of conditional heteroskedasticity
