4a08cc7c0c5ad88f17835d0d1033fad24755a914
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-03-19 17:09:14 +0800

    Add files via upload

---
+++ qs_sat/advanced/lin_reg_sklearn.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import linear_model
+
+plt.style.use('seaborn')
+
+if __name__ == '__main__':
+    N = 500
+    split = int(.8*N)
+
+    # Set the intercept and slope of the univariate linear regression simulated data
+    alpha = 2.0
+    beta = 3.0
+
+    # Set the mean and variance of the randomly distributed noise in the simulated dataset
+    eps_mu = 0.0
+    eps_sigma = 30.0
+
+    # Set the mean and variance of the X data
+    X_mu = 0.0
+    X_sigma = 10.0
+
+    # Create the error/noise, X and y data
+    eps = np.random.normal(loc=eps_mu, scale=eps_sigma, size=N)
+    X = np.random.normal(loc=X_mu, scale=X_sigma, size=N)
+    y = alpha + beta*X + eps
+    X = X.reshape(-1, 1) # Needed to avoid deprecation warning, change to 500*1 matrix like
+
+    # Could use train_test_split() function in sklearn instead
+    X_train = X[:split]
+    X_test = X[split:]
+    y_train = y[:split]
+    y_test = y[split:]
+
+    # Open a scikit-learn linear regression model and fit it to the training data
+    lr_model = linear_model.LinearRegression()
+    lr_model.fit(X_train, y_train)
+
+    print( 
+        "Estimated intercept, slope: %0.6f, %0.6f" % (lr_model.intercept_, lr_model.coef_[0])
+    )
+
+    plt.scatter(X_test, y_test)
+    plt.plot( X_test, lr_model.predict(X_test), color='black', linewidth=1.0)
+    plt.xlabel("X")
+    plt.ylabel("y")
+    plt.show()
