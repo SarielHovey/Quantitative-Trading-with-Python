2d90d0b985f9997ec4391f0d94894d0dda4d4419
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-03-27 17:01:11 +0800

    Add files via upload

--- qs_sat/advanced/example_K-Mean_Cluster.py
+++ qs_sat/advanced/example_K-Mean_Cluster.py
@@ -50,19 +50,24 @@ def simulate_KMeans():
     ax2.set_title("K-Means with $k=4$")
     plt.show()
 
-def get_open_normalised_prices(symbol, start, end):
+def get_open_normalised_prices(symbol, start, end, query_switch=False):
     """
     Obtains a pandas DataFrame containing open normalised prices for high, low and close for a particular equities symbol from DB. That is, it creates High/Open, Low/Open and Close/Open columns.
+    ------
+    Default query_switch=False, will query data from DB.
+    If query_switch=True, then symbol should be a DataFrame with same format with DB output.
     """
-    tu = TuShare()
-    df = tu.get_daily_data_sql(symbol, startdate=start, enddate=end)
+    if query_switch == False:
+        tu = TuShare()
+        df = tu.get_daily_data_sql(symbol, startdate=start, enddate=end)
+    else:
+        df = symbol
     df["H/O"] = df["high_price"]/df["open_price"]
     df["L/O"] = df["low_price"]/df["open_price"]
     df["C/O"] = df["close_price"]/df["open_price"]
-    df.drop([
+    return df.drop([
         'open_price','high_price','low_price','close_price','volume','adj_factor'
-    ], axis=1, inplace=True)
-    return df
+    ], axis=1)
 
 def plot_candlesticks(data, since):
     """
@@ -97,11 +102,94 @@ def plot_candlesticks(data, since):
     )
     plt.show()
 
+def plot_3d_normalised_candles(data):
+    """
+    Plot a 3D scatterchart of the open-normalised bars highlighting the separate clusters by colour.
+    """
+    fig = plt.figure(figsize=(12, 9))
+    ax = Axes3D(fig, elev=21, azim=-136)
+    ax.scatter(
+        data["H/O"], data["L/O"], data["C/O"],
+        c=labels.astype(np.float)
+    )
+    ax.set_xlabel('High/Open')
+    ax.set_ylabel('Low/Open')
+    ax.set_zlabel('Close/Open')
+    plt.show()
 
+def plot_cluster_ordered_candles(data):
+    """
+    Plot a candlestick chart ordered by cluster membership with the dotted blue line representing each cluster boundary.
+    """
+    # Set the format for the axis to account for dates correctly, particularly Monday as a major tick
+    mondays = WeekdayLocator(MONDAY)
+    alldays = DayLocator()
+    weekFormatter = DateFormatter("")
+    fig, ax = plt.subplots(figsize=(16,4))
+    ax.xaxis.set_major_locator(mondays)
+    ax.xaxis.set_minor_locator(alldays)
+    ax.xaxis.set_major_formatter(weekFormatter)
+    # Sort the data by the cluster values and obtain a separate DataFrame listing the index values at which the cluster boundaries change
+    df = copy.deepcopy(data)
+    df.sort_values(by="Cluster", inplace=True)
+    df.reset_index(inplace=True)
+    df["clust_index"] = df.index
+    df["clust_change"] = df["Cluster"].diff()
+    change_indices = df[df["clust_change"] != 0]
+    # Plot the OHLC chart with cluster-ordered "candles"
+    csticks = candlestick_ohlc(
+        ax, df[
+            ["clust_index", 'open_price', 'high_price', 'low_price', 'close_price']
+        ].values, width=0.6,colorup='#000000', colordown='#ff0000'
+    )
+    # Add each of the cluster boundaries as a blue dotted line
+    for row in change_indices.iterrows():
+        plt.axvline(
+            row[1]["clust_index"],
+            linestyle="dashed", c="blue"
+        )
+    plt.xlim(0, len(df))
+    plt.setp(
+        plt.gca().get_xticklabels(),
+        rotation=45, horizontalalignment='right'
+    )
+    plt.show()
+
+def create_follow_cluster_matrix(data, k=5):
+    """
+    Creates a k x k matrix, where k is the number of clusters that shows when cluster j follows cluster i.
+    """
+    data["ClusterTomorrow"] = data["Cluster"].shift(-1)
+    data.dropna(inplace=True)
+    data["ClusterTomorrow"] = data["ClusterTomorrow"].apply(int)
+    data["ClusterMatrix"] = list(
+        zip(data["Cluster"], data["ClusterTomorrow"])
+    )
+    # create a frequency distribution of ClusterToday--ClusterTomorrow pairs
+    cmvc = data["ClusterMatrix"].value_counts()
+    clust_mat = np.zeros( (k, k) )
+    for row in cmvc.iteritems():
+        clust_mat[row[0]] = row[1]*100.0/len(data)
+    print("Cluster Follow-on Matrix:")
+    print(clust_mat)
 
 if __name__ == "__main__":
     tu = TuShare()
     start = datetime.datetime(2018, 1, 1).strftime('%Y-%m-%d %H:%M:%S')
     end = datetime.datetime(2020, 3, 26).strftime('%Y-%m-%d %H:%M:%S')
     ZGYH = tu.get_daily_data_sql('601988',startdate=start,enddate=end)
+
     plot_candlesticks(ZGYH, datetime.datetime(2018, 3, 26))
+    # Carry out K-Means clustering with five clusters on the three-dimensional data H/O, L/O and C/O
+    ZGYH_norm = get_open_normalised_prices(ZGYH, start, end, query_switch=True)
+    k = 5
+    km = KMeans(n_clusters=k)
+    km.fit(ZGYH_norm)
+    labels = km.labels_
+    ZGYH["Cluster"] = labels
+    # Plot the 3D normalised candles using H/O, L/O, C/O
+    plot_3d_normalised_candles(ZGYH_norm)
+    # Plot the full OHLC candles re-ordered into their respective clusters
+    plot_cluster_ordered_candles(ZGYH)
+    # Create and output the cluster follow-on matrix
+    create_follow_cluster_matrix(ZGYH)
