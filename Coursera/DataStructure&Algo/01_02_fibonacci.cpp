5757764baa8abe009a8e57ca7634463009155baf
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-12-02 17:18:41 +0800

    Create 01_02_fibonacci.cpp

---
+++ Coursera/DataStructure&Algo/01_02_fibonacci.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+int fibonacci_naive(int n) {
+    if (n <= 1)
+        return n;
+
+    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+}
+
+int fibonacci_fast(int n) {
+    if (n > 1) {
+        std::vector<int> ve(n+1);
+        ve[0] = 0;
+        ve[1] = 1;
+        for (int i=2; i!=(n+1); i++) {
+            ve[i] = ve[i-1] + ve[i-2];
+        }
+        return *ve.rbegin();
+    }
+    else if (n = 1) {
+        return 1;
+    }
+    else {
+        return 1;
+    }
+}
+
+void test_solution() {
+    assert(fibonacci_fast(3) == 2);
+    assert(fibonacci_fast(10) == 55);
+    for (int n = 0; n < 20; ++n)
+        assert(fibonacci_fast(n) == fibonacci_naive(n));
+}
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    //std::cout << fibonacci_naive(n) << '\n';
+    //test_solution();
+    std::cout << fibonacci_fast(n) << '\n';
+    return 0;
+}
