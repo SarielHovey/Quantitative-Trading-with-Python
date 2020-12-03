e6471f4bc213e3252cc56b64efd93eb263cb4f83
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-12-03 16:41:41 +0800

    Update 01_02_fibonacci_huge.cpp

--- Coursera/DataStructure&Algo/01_02_fibonacci_huge.cpp
+++ Coursera/DataStructure&Algo/01_02_fibonacci_huge.cpp
@@ -8,24 +8,24 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     long long previous = 0;
     long long current  = 1;
 
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
+    for (long long i = 0; i != n - 1; ++i) {
+        long long tmp_previous = previous % m;
+        previous = current % m;
+        current = (tmp_previous + current) % m;
     }
 
-    return current % m ;
+    return current;
 }
 
-long long get_fibo_huge_fast(long long n, long long m) {
+long long unsigned get_fibo_huge_fast(long long unsigned n, long long unsigned m) {
     if (n > 1) {
-        std::vector<long long> ve(n+1);
+        std::vector<long long unsigned> ve(n+1);
         ve[0] = 0;
         ve[1] = 1;
-        for (long long i=2; i!=(n+1); i++) {
-            ve[i] = ve[i-1] + ve[i-2];
+        for (long long unsigned i=2; i!=(n+1); i++) {
+            ve[i] = (ve[i-1] % m + ve[i-2] % m) % m;
         }
-        return *ve.rbegin() % m ;
+        return *ve.rbegin();
     }
     else {
         return n;
@@ -33,7 +33,7 @@ long long get_fibo_huge_fast(long long n, long long m) {
 }
 
 int main() {
-    long long n, m;
+    long long unsigned n, m;
     std::cin >> n >> m;
     //std::cout << get_fibonacci_huge_naive(n, m) << '\n';
     std::cout << get_fibo_huge_fast(n, m) << "\n";
