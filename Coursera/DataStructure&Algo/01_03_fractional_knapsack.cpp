3b1c25ab6c4c2c1b6a04bc1905fbb9c78dc39fa2
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-12-08 17:18:24 +0800

    Update 01_03_fractional_knapsack.cpp

--- Coursera/DataStructure&Algo/01_03_fractional_knapsack.cpp
+++ Coursera/DataStructure&Algo/01_03_fractional_knapsack.cpp
@@ -8,46 +8,51 @@ struct item {
     int weight = 0;
     int value = 0;
     double rank = 0.0; // wight / value
-    bool operator< (const item& another) {
+    
+    item(int w, int v) {
+        weight = w;
+        value = v;
+        rank = v / w;
+    }
+    bool operator< (item& another) {
         return this->rank < another.rank;
     }
 };
 
-
-double get_optimal_value(const int& capacity, const vector<item>& items) {
+double get_optimal_value(const int capacity, const vector<item>& items) {
     double value = 0.0;
     int load = 0;
     vector<item>::const_reverse_iterator itr;
 
-    std::sort(items.begin(),items.end());
     for ( itr=items.crbegin(); itr!=items.crend(); itr++) {
         if (((*itr).weight + load) < capacity) {
             load += (*itr).weight;
             value += (*itr).value;
         }
-        else if (((*itr).weight + load) == capacity) {
-            return (value + (*itr).value);
-        }
         else {
-            int weight_gap = capacity - load;
+            double weight_gap = capacity - load;
             double value_part = weight_gap / ((*itr).weight) * ((*itr).value);
             value += value_part;
+            std::cout << "In else block" << "\n";
+            return value;
         }
     }
-    
-    return value;
 }
 
 int main() {
   int n;
   int capacity;
   std::cin >> n >> capacity;
-  vector<item> items(n);
+  
+  int w=0, v=0;
+  vector<item> items;
+  
   for (int i = 0; i != n; i++) {
-    std::cin >> items[i].value >> items[i].weight;
-    items[i].rank = items[i].value / items[i].weight;
+    std::cin >> v >> w;
+    items.insert(items.begin(), item(w,v));
   }
-
+  std::sort(items.begin(),items.end()); //sorted in rank
+  
   double optimal_value = get_optimal_value(capacity, items);
 
   std::cout.precision(10);
