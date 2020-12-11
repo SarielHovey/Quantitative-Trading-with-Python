844a4a107868360e7f6699730ecf69d7602ddda8
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-12-11 11:19:05 +0800

    Create 01_03_largest_number.cpp

---
+++ Coursera/DataStructure&Algo/01_03_largest_number.cpp
@@ -0,0 +1,52 @@
+/*
+Description:
+    Given a list of int (e.g. 114,514,810,893,24,1919,3), return the largest int while combining them.
+*/
+
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <list>
+#include <string>
+
+using std::list;
+using std::string;
+using std::stringstream;
+using std::stoi;
+
+bool leq(const int& x1, const int& x2) {
+    stringstream s1, s2;
+    
+    s1 << x1 << x2;
+    s2 << x2 << x1;
+    
+    
+    return stoi(s1.str()) < stoi(s2.str());
+}
+
+string largest_number(list<int>& a) {
+    
+    a.sort(leq);
+  
+    stringstream ret;
+    for (list<int>::const_reverse_iterator itr = a.crbegin(); itr != a.crend(); itr++) {
+        ret << *itr;    
+    }
+  
+    string result;
+    ret >> result;
+    return result;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    list<int> a;
+  
+    int tmp;
+    for (int i=0; i!=n; i++) {
+        std::cin >> tmp;
+        a.push_back(tmp);
+    }
+    std::cout << largest_number(a);
+}
